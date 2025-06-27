ac7ea7d472e432731ab529038cc0b40e32b6a5e3
ivy <ivy.chemutai@student.moringaschool.com>
2025-06-27 16:29:27 +0300

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+const element = document.createElement("div");
+// Optionally remove the existing main element
+const main = document.getElementById("main");
+if (main) {
+  main.remove();
+}
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set its id
+newHeader.id = "victory";
+
+// Set its text content
+newHeader.textContent = "Lavi is the champion";
+
+// Append the new header to the body
+document.body.append(newHeader);
